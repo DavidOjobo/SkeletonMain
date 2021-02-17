e73d2a4e627aed1afa9c7f7ec9d6e28735955a81
David Ojobo <P2533900@my365.dmu.ac.uk>
2021-02-17 17:37:11 +0000

    Created classes and test cases for each property in tblQuality.

--- ClassLibrary/clsQuality.cs
+++ ClassLibrary/clsQuality.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsQuality
     {
+        public bool Defective { get; set; }
+        public DateTime Date { get; set; }
+        public char Grade { get; set; }
+        public int BatchNo { get; set; }
+        public int StaffID { get; set; }
+        public int ProductNo { get; set; }
     }
 }
--- Testing5/tstQuality.cs
+++ Testing5/tstQuality.cs
@@ -8,12 +8,78 @@ namespace Testing5
     public class tstQuality
     {
         [TestMethod]
-        public void TInstanceOK()
+        public void InstanceOK()
         {
             //creates instance of class
             clsQuality QualityControl = new clsQuality();
             //tests to see if it exists
             Assert.IsNotNull(QualityControl);
         }
+        [TestMethod]
+        public void DefectivePropertyOK()
+        {
+            //create instance of class
+            clsQuality QualityControl = new clsQuality();
+            //create test data
+            Boolean TestData = true;
+            //assign data to property
+            QualityControl.Defective = TestData;
+            //test to see if values are the same
+            Assert.AreEqual(QualityControl.Defective, TestData);
+
+        }
+        [TestMethod]
+        public void DatePropertyOK()
+        {
+            //create instance of class
+            clsQuality QualityControl = new clsQuality();
+            //create test with datatype DateTime
+            DateTime TestData = DateTime.Now.Date;
+            //Assign data with property
+            QualityControl.Date = TestData;
+            //Test to see if they are equal
+            Assert.AreEqual(QualityControl.Date, TestData);
+        }
+        [TestMethod]
+        public void GradePropertyOK()
+        {
+            //create class instance
+            clsQuality QualityControl = new clsQuality();
+            //create test for property
+            Char TestData = 'A';
+            //assign data to property
+            QualityControl.Grade = TestData;
+            //test for if they are equal
+            Assert.AreEqual(QualityControl.Grade, TestData);
+
+        }
+        [TestMethod]
+        public void BatchNoPropertyOK()
+        {
+            clsQuality QualityControl = new clsQuality();
+            Int32 DataTest = 1;
+            QualityControl.BatchNo = DataTest;
+            Assert.AreEqual(QualityControl.BatchNo, DataTest);
+        }
+        [TestMethod]
+        public void StaffIDPropertyOK()
+        {
+            clsQuality QualityControl = new clsQuality();
+            Int32 DataTest = 1;
+            QualityControl.StaffID = DataTest;
+            Assert.AreEqual(QualityControl.StaffID, DataTest);
+
+        }
+        [TestMethod]
+        public void ProductNoPropertyOK()
+        {
+            clsQuality QualityControl = new clsQuality();
+            Int32 DataTest = 1;
+            QualityControl.ProductNo = DataTest;
+            Assert.AreEqual(QualityControl.ProductNo, DataTest);
+        }
+
+        
+
     }
 }
