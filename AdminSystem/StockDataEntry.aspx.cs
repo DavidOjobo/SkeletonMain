b615ef720b4280bf650cbb4da1828945c504b0e0
Daniel Ojobo <P2540171@my365.dmu.ac.uk>
2021-02-21 17:20:32 +0000

    txtProductno

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using ClassLibrary;
 
+
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
@@ -13,21 +14,8 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     }
 
-    protected void btnOk_Click(object sender, EventArgs e)
+    protected void btnOK_Click(object sender, EventArgs e)
     {
-        //create a new instance of clsStock
-        clsStock aStock = new clsStock();
-        aStock.ProductNo = txtProductNo.Text;
-        //navigate to the viewers' page
-        Response.Redirect("StockViewer.aspx");
+        
     }
-}
-
-public class clsStock
-{
-    public clsStock()
-    {
-    }
-
-    public object ProductNo { get; internal set; }
 }
