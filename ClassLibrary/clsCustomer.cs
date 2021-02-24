2048a3970dc92d9af761be36dc169238740a55d7
David Ojobo <P2533900@my365.dmu.ac.uk>
2021-02-24 16:21:23 +0000

    Test for Customer

---
+++ ClassLibrary/clsCustomer.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary
+{
+    public class clsCustomer
+    {
+        public clsCustomer()
+        {
+        }
+
+        public string ProductNo { get; set; }
+    }
+}
---
+++ Testing2/tstCustomer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer Customer = new clsCustomer();
+            Assert.IsNotNull(Customer);
+        }
+        [TestMethod]
+        public void ProductNoOK()
+        {
+            clsCustomer Customer = new clsCustomer();
+            String TestData = "2a";
+            Customer.ProductNo = TestData;
+            Assert.AreEqual(Customer.ProductNo, TestData);
+        }
+
+    }
+}
