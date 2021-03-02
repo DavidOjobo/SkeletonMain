e39205599e033e0406caed3193df7d33d9539ae7
P2535229 <P2535229@GWHXX652WIN004W>
2021-03-02 18:05:48 +0000

    Class Customer Test

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,4 +1,6 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
@@ -7,5 +9,12 @@
         }
 
         public string ProductNo { get; set; }
+        public int CustomerID { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public int OrderNo { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime Date { get; set; }
     }
 }
