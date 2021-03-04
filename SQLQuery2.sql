faeff9bbfc0aaef61b8d4914879b1fdca4ed7e44
P2535229 <P2535229@GWHXX284WIN002W>
2021-03-04 22:33:35 +0000

    Week 20

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+﻿USE [p2416227]
+GO
+
+DECLARE	@return_value Int
+
+EXEC	@return_value = [dbo].[sproc_tblProduct_FJ_Delete]
+		@ProductNo = 3
+
+SELECT	@return_value as 'Return Value'
+
+GO
---
+++ SQLQuery3.sql
@@ -0,0 +1,10 @@
+﻿USE [p2416227]
+GO
+
+DECLARE	@return_value Int
+
+EXEC	@return_value = [dbo].[sproc_tblCustomer_FJ_FilterByProductNo]
+
+SELECT	@return_value as 'Return Value'
+
+GO
